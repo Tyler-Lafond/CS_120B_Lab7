3f6b357254d665b4c249f0e4e3862979406d2b2b
Tyler-Lafond <39169194+Tyler-Lafond@users.noreply.github.com>
2021-10-21 12:01:16 -0700

    Add files via upload

---
+++ Lab7_LCD/turnin/tlafo001_lab6_part2.c
@@ -0,0 +1,189 @@
+/*	Author: tlafo001
+ *  Partner(s) Name: 
+ *	Lab Section: 022
+ *	Assignment: Lab # 6  Exercise # 2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#include "timer.h"
+#endif
+
+enum LED_States { LED_SMStart, LED_Change_Unpress, LED_Pause_Press, LED_Pause_Unpress, LED_Change_Press } LED_State;
+unsigned char tempB;
+unsigned char direct; // 0x00 for shift left and 0x01 for shift right
+
+void TickLED()
+{
+	switch(LED_State)
+	{
+		case LED_SMStart:
+			tempB = 0x01;
+			direct = 0x00;
+			LED_State = LED_Change_Unpress;
+			break;
+		case LED_Change_Unpress:
+			if ((PINA & 0x01) == 0x00)
+			{
+				LED_State = LED_Pause_Press;
+			}
+			else if ((PINA & 0x01) == 0x01)
+			{
+				if (tempB == 0x04)
+				{
+					tempB = tempB >> 1;
+					direct = 0x01;
+				}
+				else if (tempB == 0x02)
+				{
+					if(direct)
+					{
+						tempB = tempB >> 1;
+					}
+					else
+					{
+						tempB = tempB << 1;
+					}
+				}
+				else if (tempB == 0x01)
+				{
+					tempB = tempB << 1;
+					direct = 0x00;
+				}
+				LED_State = LED_Change_Unpress;
+			}
+			break;
+		case LED_Pause_Press:
+			if ((PINA & 0x01) == 0x01)
+			{
+				LED_State = LED_Pause_Unpress;
+			}
+			else if ((PINA & 0x01) == 0x00)
+			{
+				LED_State = LED_Pause_Press;
+			}
+			break;
+		case LED_Pause_Unpress:
+			if ((PINA & 0x01) == 0x00)
+			{
+				if (tempB == 0x04)
+				{
+					tempB = tempB >> 1;
+					direct = 0x01;
+				}
+				else if (tempB == 0x02)
+				{
+					if(direct)
+					{
+						tempB = tempB >> 1;
+					}
+					else
+					{
+						tempB = tempB << 1;
+					}
+				}
+				else if (tempB == 0x01)
+				{
+					tempB = tempB << 1;
+					direct = 0x00;
+				}
+				LED_State = LED_Change_Press;
+			}
+			else if ((PINA & 0x01) == 0x01)
+			{
+				LED_State = LED_Pause_Unpress;
+			}
+			break;
+		case LED_Change_Press:
+			if ((PINA & 0x01) == 0x01)
+			{
+				if (tempB == 0x04)
+				{
+					tempB = tempB >> 1;
+					direct = 0x01;
+				}
+				else if (tempB == 0x02)
+				{
+					if(direct)
+					{
+						tempB = tempB >> 1;
+					}
+					else
+					{
+						tempB = tempB << 1;
+					}
+				}
+				else if (tempB == 0x01)
+				{
+					tempB = tempB << 1;
+					direct = 0x00;
+				}
+				LED_State = LED_Change_Unpress;
+			}
+			else if ((PINA & 0x01) == 0x00)
+			{
+				if (tempB == 0x04)
+				{
+					tempB = tempB >> 1;
+					direct = 0x01;
+				}
+				else if (tempB == 0x02)
+				{
+					if(direct)
+					{
+						tempB = tempB >> 1;
+					}
+					else
+					{
+						tempB = tempB << 1;
+					}
+				}
+				else if (tempB == 0x01)
+				{
+					tempB = tempB << 1;
+					direct = 0x00;
+				}
+				LED_State = LED_Change_Press;
+			}
+			break;
+		default:
+			LED_State = LED_SMStart;
+			break;
+	}
+
+	switch(LED_State)
+	{
+		case LED_Change_Unpress:
+			PORTB = tempB;
+			break;
+		case LED_Pause_Press:
+			break;
+		case LED_Pause_Unpress:
+			break;
+		case LED_Change_Press:
+			PORTB = tempB;
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	TimerSet(300);
+	TimerOn();
+    /* Insert your solution below */
+	tempB = 0x00;
+    while (1) {
+	TickLED();
+	while(!TimerFlag);
+	TimerFlag = 0;
+    }
+    return 1;
+}
---
+++ Lab7_LCD/turnin/tlafo001_lab6_part3.c
@@ -0,0 +1,262 @@
+/*	Author: tlafo001
+ *  Partner(s) Name: 
+ *	Lab Section: 022
+ *	Assignment: Lab # 6  Exercise # 3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#include "timer.h"
+#endif
+
+unsigned char cnt;
+unsigned char prev;
+unsigned char time;
+
+enum Cnt_States { Cnt_SMStart, Cnt_Unpress, Cnt_Press_a0, Cnt_Press_a1, Cnt_Press_Both, Cnt_Unpress_One } Cnt_State;
+
+void TickCnt()
+{
+	switch(Cnt_State)
+	{
+		case Cnt_SMStart:
+			cnt = 0x07;
+			PORTB = cnt;
+			time = 0;
+			Cnt_State = Cnt_Unpress;
+			break;
+		case Cnt_Unpress:
+			if ((PINA & 0x03) == 0x02)
+			{
+				if (cnt != 0x09)
+				{
+					cnt++;
+					PORTB = cnt;
+				}
+				Cnt_State = Cnt_Press_a0;
+			}
+			else if ((PINA & 0x03) == 0x01)
+			{
+				if (cnt != 0x00)
+				{
+					cnt--;
+					PORTB = cnt;
+				}
+				Cnt_State = Cnt_Press_a1;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				cnt = 0x00;
+				PORTB = cnt;
+				Cnt_State = Cnt_Press_Both;
+			}
+			else if ((PINA & 0x03) == 0x03)
+			{
+				Cnt_State = Cnt_Unpress;
+			}
+			break;
+		case Cnt_Press_a0:
+			if ((PINA & 0x03) == 0x03)
+			{
+				time = 0;
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				cnt = 0x00;
+				PORTB = cnt;
+				time = 0;
+				Cnt_State = Cnt_Press_Both;
+			}
+			else if ((PINA & 0x03) == 0x01)
+			{
+				if (cnt != 0x00)
+				{
+					cnt--;
+					PORTB = cnt;
+				}
+				time = 0;
+				Cnt_State = Cnt_Press_a1;
+			}
+			else if ((PINA & 0x03) == 0x02)
+			{
+				if ((time % 10 == 0) && (cnt != 0x09))
+				{
+					cnt++;
+					PORTB = cnt;
+				}
+				Cnt_State = Cnt_Press_a0;
+			}
+			break;
+		case Cnt_Press_a1:
+			if ((PINA & 0x03) == 0x03)
+			{
+				time = 0;
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				cnt = 0x00;
+				time = 0;
+				PORTB = cnt;
+				Cnt_State = Cnt_Press_Both;
+			}
+			else if ((PINA & 0x03) == 0x02)
+			{
+				if (cnt != 0x09)
+				{
+					cnt++;
+					PORTB = cnt;
+				}
+				time = 0;
+				Cnt_State = Cnt_Press_a0;
+			}
+			else if ((PINA & 0x03) == 0x01)
+			{
+				if ((time % 10 == 0) && (cnt != 0x00))
+				{
+					cnt--;
+					PORTB = cnt;
+				}
+				Cnt_State = Cnt_Press_a1;
+			}
+			break;
+		case Cnt_Press_Both:
+			if ((PINA & 0x03) == 0x03)
+			{
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x02 || (PINA & 0x03) == 0x01)
+			{
+				Cnt_State = Cnt_Unpress_One;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				Cnt_State = Cnt_Press_Both;
+			}
+			break;
+		case Cnt_Unpress_One:
+			if ((PINA & 0x03) == 0x03)
+			{
+				time = 0;
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				cnt = 0x00;
+				time = 0;
+				PORTB = cnt;
+				Cnt_State = Cnt_Press_Both;
+			}
+			else if (prev != (PINA & 0x03))
+			{
+				if (prev == 0x01)
+				{
+					if (cnt != 0x09)
+					{
+						cnt++;
+						time = 0;
+						PORTB = cnt;
+					}
+					Cnt_State = Cnt_Press_a0;
+				}
+				else if (prev == 0x02)
+				{
+					if (cnt != 0x00)
+					{
+						cnt--;
+						time = 0;
+						PORTB = cnt;
+					}
+					Cnt_State = Cnt_Press_a1;
+				}
+			}
+			else if (prev == (PINA & 0x03))
+			{
+				if (prev == 0x02)
+				{
+					if ((cnt != 0x09) && (time % 10 == 0))
+					{
+						cnt++;
+						PORTB = cnt;
+					}
+				}
+				else if (prev == 0x01)
+				{
+					if ((cnt != 0x00) && (time % 10 == 0))
+					{
+						cnt--;
+						PORTB = cnt;
+					}
+				}
+				Cnt_State = Cnt_Unpress_One;
+			}
+			break;
+		default:
+			Cnt_State = Cnt_SMStart;
+			break;
+	}
+
+	switch(Cnt_State)
+	{
+		case Cnt_Unpress:
+			break;
+		case Cnt_Press_a0:
+			if (time != 0xFF)
+			{
+				time++;
+			}
+			else
+			{
+				time = 0x06;
+			}
+			break;
+		case Cnt_Press_a1:
+			if (time != 0xFF)
+			{
+				time++;
+			}
+			else
+			{
+				time = 0x06;
+			}
+			break;
+		case Cnt_Press_Both:
+			break;
+		case Cnt_Unpress_One:
+			if (time != 0xFF)
+			{
+				time++;
+			}
+			else
+			{
+				time = 0x06;
+			}
+			prev = (PINA & 0x03);
+			break;
+		default:
+			break;
+	}
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+    /* Insert your solution below */
+	PORTB = 0x07;
+	TimerSet(100);
+	TimerOn();
+	Cnt_State = Cnt_SMStart;
+
+    while (1) {
+	TickCnt();
+	while(!TimerFlag);
+	TimerFlag = 0;
+    }
+    return 1;
+}
