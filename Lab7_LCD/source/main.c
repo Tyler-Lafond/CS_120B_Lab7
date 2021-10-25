03d69259d7abb255bad9c094340447b837c3ebd7
Tyler Lafond <tlafo001@wch136-18.cs.ucr.edu>
2021-10-24 17:06:01 -0700

    Fixed Lab 7 part 1

--- Lab7_LCD/source/main.c
+++ Lab7_LCD/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: tlafo001
  *  Partner(s) Name: 
  *	Lab Section: 022
- *	Assignment: Lab # 7  Exercise # 2
+ *	Assignment: Lab # 7  Exercise # 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,206 +14,291 @@
 #include "io.h"
 #endif
 
-enum LED_States { LED_SMStart, LED_Change_Unpress, LED_Pause_Press, LED_Pause_Unpress, LED_Change_Press, LED_Victory_Press, LED_Victory_Unpress } LED_State;
-unsigned char tempB;
 unsigned char cnt;
-unsigned char tempCnt;
-unsigned const char winner[8] = "WINNER!!";
-unsigned const char* win = winner;
-unsigned char direct; // 0x00 for shift left and 0x01 for shift right
+unsigned char temp;
+unsigned char prev;
+unsigned char time;
 
-void TickLED()
+enum Cnt_States { Cnt_SMStart, Cnt_Unpress, Cnt_Press_a0, Cnt_Press_a1, Cnt_Press_Both, Cnt_Unpress_One } Cnt_State;
+
+void TickCnt()
 {
-	switch(LED_State)
+	switch(Cnt_State)
 	{
-		case LED_SMStart:
-			tempB = 0x01;
-			direct = 0x00;
-			cnt = 5;
+		case Cnt_SMStart:
+			cnt = 0x00;
+			PORTB = cnt;
+			temp = cnt + '0';
 			LCD_ClearScreen();
 			LCD_Cursor(1);
-			LCD_WriteData(cnt + '0');
-			LED_State = LED_Change_Unpress;
+			LCD_WriteData(temp);
+			time = 0;
+			Cnt_State = Cnt_Unpress;
 			break;
-		case LED_Change_Unpress:
-			if ((PINA & 0x01) == 0x00)
+		case Cnt_Unpress:
+			if ((PINA & 0x03) == 0x02)
 			{
-				if (tempB == 0x02)
+				if (cnt != 0x09)
 				{
 					cnt++;
-					if (cnt <= 9)
-					{
-						LCD_ClearScreen();
-						LCD_Cursor(1);
-						LCD_WriteData(cnt + '0');
-					}
-				}
-				else
-				{
-					if (cnt > 0x00)
-					{
-						cnt--;
-					}
+					PORTB = cnt;
+					temp = cnt + '0';
 					LCD_ClearScreen();
 					LCD_Cursor(1);
-					LCD_WriteData(cnt + '0');
+					LCD_WriteData(temp);
 				}
-				
-				if (cnt >= 0x09)
-				{
-					LCD_DisplayString(1, win);
-					LED_State = LED_Victory_Press;
-				}
-				else
-				{
-					LED_State = LED_Pause_Press;
-				}
+				Cnt_State = Cnt_Press_a0;
 			}
-			else if ((PINA & 0x01) == 0x01)
+			else if ((PINA & 0x03) == 0x01)
 			{
-				if (tempB == 0x04)
+				if (cnt != 0x00)
 				{
-					tempB = tempB >> 1;
-					direct = 0x01;
+					cnt--;
+					PORTB = cnt;
+					temp = cnt + '0';
+					LCD_ClearScreen();
+					LCD_Cursor(1);
+					LCD_WriteData(temp);
 				}
-				else if (tempB == 0x02)
-				{
-					if(direct)
-					{
-						tempB = tempB >> 1;
-					}
-					else
-					{
-						tempB = tempB << 1;
-					}
-				}
-				else if (tempB == 0x01)
-				{
-					tempB = tempB << 1;
-					direct = 0x00;
-				}
-				LED_State = LED_Change_Unpress;
+				Cnt_State = Cnt_Press_a1;
 			}
-			break;
-		case LED_Pause_Press:
-			if ((PINA & 0x01) == 0x01)
+			else if ((PINA & 0x03) == 0x00)
 			{
-				LED_State = LED_Pause_Unpress;
-			}
-			else if ((PINA & 0x01) == 0x00)
-			{
-				LED_State = LED_Pause_Press;
-			}
-			break;
-		case LED_Pause_Unpress:
-			if ((PINA & 0x01) == 0x00)
-			{
-				tempB = 0x01;
-				direct = 0x00;
-				LED_State = LED_Change_Press;
-			}
-			else if ((PINA & 0x01) == 0x01)
-			{
-				LED_State = LED_Pause_Unpress;
-			}
-			break;
-		case LED_Change_Press:
-			if ((PINA & 0x01) == 0x01)
-			{
-				if (tempB == 0x04)
-				{
-					tempB = tempB >> 1;
-					direct = 0x01;
-				}
-				else if (tempB == 0x02)
-				{
-					if(direct)
-					{
-						tempB = tempB >> 1;
-					}
-					else
-					{
-						tempB = tempB << 1;
-					}
-				}
-				else if (tempB == 0x01)
-				{
-					tempB = tempB << 1;
-					direct = 0x00;
-				}
-				LED_State = LED_Change_Unpress;
-			}
-			else if ((PINA & 0x01) == 0x00)
-			{
-				if (tempB == 0x04)
-				{
-					tempB = tempB >> 1;
-					direct = 0x01;
-				}
-				else if (tempB == 0x02)
-				{
-					if(direct)
-					{
-						tempB = tempB >> 1;
-					}
-					else
-					{
-						tempB = tempB << 1;
-					}
-				}
-				else if (tempB == 0x01)
-				{
-					tempB = tempB << 1;
-					direct = 0x00;
-				}
-				LED_State = LED_Change_Press;
-			}
-			break;
-		case LED_Victory_Press:
-			if ((PINA & 0x01) == 0x01)
-			{
-				LED_State = LED_Victory_Unpress;
-			}
-			else if ((PINA & 0x01) == 0x00)
-			{
-				LED_State = LED_Victory_Press;
-			}
-			break;
-		case LED_Victory_Unpress:
-			if ((PINA & 0x01) == 0x00)
-			{
-				cnt = 5;
-				tempB = 0x01;
+				cnt = 0x00;
+				PORTB = cnt;
+				temp = cnt + '0';
 				LCD_ClearScreen();
 				LCD_Cursor(1);
-				LCD_WriteData(cnt + '0');
-				LED_State = LED_Change_Press;
+				LCD_WriteData(temp);
+				Cnt_State = Cnt_Press_Both;
 			}
-			else if ((PINA & 0x01) == 0x01)
+			else if ((PINA & 0x03) == 0x03)
 			{
-				LED_State = LED_Victory_Unpress;
+				Cnt_State = Cnt_Unpress;
+			}
+			break;
+		case Cnt_Press_a0:
+			if ((PINA & 0x03) == 0x03)
+			{
+				time = 0;
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				cnt = 0x00;
+				PORTB = cnt;
+				temp = cnt + '0';
+				LCD_ClearScreen();
+				LCD_Cursor(1);
+				LCD_WriteData(temp);
+				time = 0;
+				Cnt_State = Cnt_Press_Both;
+			}
+			else if ((PINA & 0x03) == 0x01)
+			{
+				if (cnt != 0x00)
+				{
+					cnt--;
+					PORTB = cnt;
+					temp = cnt + '0';
+					LCD_ClearScreen();
+					LCD_Cursor(1);
+					LCD_WriteData(temp);
+				}
+				time = 0;
+				Cnt_State = Cnt_Press_a1;
+			}
+			else if ((PINA & 0x03) == 0x02)
+			{
+				if ((time % 10 == 0) && (cnt != 0x09))
+				{
+					cnt++;
+					PORTB = cnt;
+					temp = cnt + '0';
+					LCD_ClearScreen();
+					LCD_Cursor(1);
+					LCD_WriteData(temp);
+				}
+				Cnt_State = Cnt_Press_a0;
+			}
+			break;
+		case Cnt_Press_a1:
+			if ((PINA & 0x03) == 0x03)
+			{
+				time = 0;
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				cnt = 0x00;
+				PORTB = cnt;
+				time = 0;
+				temp = cnt + '0';
+				LCD_ClearScreen();
+				LCD_Cursor(1);
+				LCD_WriteData(temp);
+				Cnt_State = Cnt_Press_Both;
+			}
+			else if ((PINA & 0x03) == 0x02)
+			{
+				if (cnt != 0x09)
+				{
+					cnt++;
+					PORTB = cnt;
+					temp = cnt + '0';
+					LCD_ClearScreen();
+					LCD_Cursor(1);
+					LCD_WriteData(temp);
+				}
+				time = 0;
+				Cnt_State = Cnt_Press_a0;
+			}
+			else if ((PINA & 0x03) == 0x01)
+			{
+				if ((time % 10 == 0) && (cnt != 0x00))
+				{
+					cnt--;
+					PORTB = cnt;
+					temp = cnt + '0';
+					LCD_ClearScreen();
+					LCD_Cursor(1);
+					LCD_WriteData(temp);
+				}
+				Cnt_State = Cnt_Press_a1;
+			}
+			break;
+		case Cnt_Press_Both:
+			if ((PINA & 0x03) == 0x03)
+			{
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x02 || (PINA & 0x03) == 0x01)
+			{
+				Cnt_State = Cnt_Unpress_One;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				Cnt_State = Cnt_Press_Both;
+			}
+			break;
+		case Cnt_Unpress_One:
+			if ((PINA & 0x03) == 0x03)
+			{
+				time = 0;
+				Cnt_State = Cnt_Unpress;
+			}
+			else if ((PINA & 0x03) == 0x00)
+			{
+				cnt = 0x00;
+				PORTB = cnt;
+				time = 0;
+				temp = cnt + '0';
+				LCD_ClearScreen();
+				LCD_Cursor(1);
+				LCD_WriteData(temp);
+				Cnt_State = Cnt_Press_Both;
+			}
+			else if (prev != (PINA & 0x03))
+			{
+				if (prev == 0x01)
+				{
+					if (cnt != 0x09)
+					{
+						cnt++;
+						PORTB = cnt;
+						time = 0;
+						temp = cnt + '0';
+						LCD_ClearScreen();
+						LCD_Cursor(1);
+						LCD_WriteData(temp);
+					}
+					Cnt_State = Cnt_Press_a0;
+				}
+				else if (prev == 0x02)
+				{
+					if (cnt != 0x00)
+					{
+						cnt--;
+						PORTB = cnt;
+						time = 0;
+						temp = cnt + '0';
+						LCD_ClearScreen();
+						LCD_Cursor(1);
+						LCD_WriteData(temp);
+					}
+					Cnt_State = Cnt_Press_a1;
+				}
+			}
+			else if (prev == (PINA & 0x03))
+			{
+				if (prev == 0x02)
+				{
+					if ((cnt != 0x09) && (time % 10 == 0))
+					{
+						cnt++;
+						PORTB = cnt;
+						temp = cnt + '0';
+						LCD_ClearScreen();
+						LCD_Cursor(1);
+						LCD_WriteData(temp);
+					}
+				}
+				else if (prev == 0x01)
+				{
+					if ((cnt != 0x00) && (time % 10 == 0))
+					{
+						cnt--;
+						PORTB = cnt;
+						temp = cnt + '0';
+						LCD_ClearScreen();
+						LCD_Cursor(1);
+						LCD_WriteData(temp);
+					}
+				}
+				Cnt_State = Cnt_Unpress_One;
 			}
 			break;
 		default:
-			LED_State = LED_SMStart;
+			Cnt_State = Cnt_SMStart;
 			break;
 	}
 
-	switch(LED_State)
+	switch(Cnt_State)
 	{
-		case LED_Change_Unpress:
-			PORTB = tempB;
+		case Cnt_Unpress:
 			break;
-		case LED_Pause_Press:
+		case Cnt_Press_a0:
+			if (time != 0xFF)
+			{
+				time++;
+			}
+			else
+			{
+				time = 0x06;
+			}
 			break;
-		case LED_Pause_Unpress:
+		case Cnt_Press_a1:
+			if (time != 0xFF)
+			{
+				time++;
+			}
+			else
+			{
+				time = 0x06;
+			}
 			break;
-		case LED_Change_Press:
-			PORTB = tempB;
+		case Cnt_Press_Both:
 			break;
-		case LED_Victory_Press:
-			break;
-		case LED_Victory_Unpress:
+		case Cnt_Unpress_One:
+			if (time != 0xFF)
+			{
+				time++;
+			}
+			else
+			{
+				time = 0x06;
+			}
+			prev = (PINA & 0x03);
 			break;
 		default:
 			break;
@@ -226,14 +311,18 @@ int main(void) {
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
-	TimerSet(300);
-	TimerOn();
     /* Insert your solution below */
-	tempB = 0x00;
+	temp = 0x00;
 	LCD_init();
+	LCD_Cursor(1);
+	LCD_WriteData(0x07);
+
+	TimerSet(100);
+	TimerOn();
+	Cnt_State = Cnt_SMStart;
 
     while (1) {
-	TickLED();
+	TickCnt();
 	while(!TimerFlag);
 	TimerFlag = 0;
     }
